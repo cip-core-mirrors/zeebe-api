6f0c6df3d56a543b6d01b8073260a23e29223ac1
Murat Cengiz <mcengiz2402@gmail.com>
2021-07-15 16:37:46 +0200

    Client connection retry longer interval

--- src/workers/github.js
+++ src/workers/github.js
@@ -4,6 +4,7 @@ const zeebe = require('zeebe-node');
 const zbc = new zeebe.ZBClient(process.env.ZEEBE_HOST, {
     retry: true,
     maxRetries: -1, // infinite retries
+    maxRetryTimeout: zeebe.Duration.seconds.of(5),
 });
 
 zbc.on('ready', () => console.log(`Client connected!`));
