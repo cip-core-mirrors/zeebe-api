cfe28869c3cac56fb3dbf8014f4a40ac5e04a757
Murat Cengiz <mcengiz2402@gmail.com>
2021-07-19 21:13:31 +0200

    Ping topology

--- src/zeebe/client.js
+++ src/zeebe/client.js
@@ -1,12 +1,28 @@
 const zeebe = require('zeebe-node');
 
-const zbc = new zeebe.ZBClient(process.env.ZEEBE_HOST, {
-    retry: true,
-    maxRetries: -1, // infinite retries
-    maxRetryTimeout: zeebe.Duration.seconds.of(5),
-});
+// See options at https://www.npmjs.com/package/zeebe-node#client-side-retry
+const zbc = new zeebe.ZBClient();
 
-zbc.on('ready', () => console.log(`Client connected!`));
-zbc.on('connectionError', () => console.log(`Client disconnected!`));
+async function onReady() {
+    await pingTopology()
+}
+
+async function onConnectionError() {
+    await pingTopology()
+}
+
+let firstCall = true
+async function pingTopology() {
+    const topology = await zbc.topology()
+    if (firstCall) {
+        console.log(JSON.stringify(topology, null, 2))
+        firstCall = false
+        const pingIntervalSeconds = process.env.ZEEBE_PING ? parseInt(process.env.ZEEBE_PING) : 60
+        setInterval(pingTopology, pingIntervalSeconds * 1000)
+    }
+}
+
+zbc.on('ready', onReady);
+zbc.on('connectionError', onConnectionError);
 
 module.exports = zbc;
